111c9ae76f61f290b3d52de96b9351d53177680b
Jörg Zieren <github@zieren.de>
2022-03-11 16:56:24 +0100

    Playing around

---
+++ main.js
@@ -0,0 +1,79 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://theogymuc.eltern-portal.org/');
+  await page.type('#inputEmail', 'eltern@zieren.de');
+  await page.type('#inputPassword', 'gvx5ZN6VMxZr3EF');
+  await Promise.all([
+    page.click('#inputPassword ~ button'),
+    page.waitForNavigation(),
+  ]);
+
+  // Build the list of teachers that have communicated.
+  await page.goto('https://theogymuc.eltern-portal.org/meldungen/kommunikation_fachlehrer');
+  const teachersList = await page.$$eval(
+    'td:nth-child(3) a[href*="meldungen/kommunikation_fachlehrer/"',
+    (anchors) => anchors.map(
+      (a) => {
+        const m = a.href.match(/.*\/([0-9]+)\//);
+        const id = m[1];
+        return {
+          'id': id,
+          'url': a.href,
+          'name': a.parentElement.parentElement.firstChild.textContent
+        }
+      }));
+  // Map of teacher ID to details (name and URL).
+  const teachers = {};
+  teachersList.forEach((t) => {
+    teachers[t['id']] = t;
+    delete t['id'];
+  });
+  console.log(JSON.stringify(teachers));
+
+  // Retrieve metadata for all threads.
+  // Map of thread ID to details (list of messages in thread).
+  const threads = {};
+  for (const [ignored, teacher] of Object.entries(teachers)) {
+    console.log('Reading threads for: ' + teacher['name']);
+    await page.goto(teacher['url']);
+    const threadsList = await page.$$eval(
+      'a[href*="meldungen/kommunikation_fachlehrer/"',
+      (anchors) => anchors.map((a) => {
+        const m = a.href.match(/.*\/([0-9]+)$/);
+        const id = m[1];
+        return {
+          'id': id,
+          'url': a.href,
+          'subject': a.textContent
+        };
+      }));
+    threadsList.forEach((t) => {
+      threads[t['id']] = t;
+      delete t['id'];
+    });
+  }
+  console.log(JSON.stringify(threads));
+  console.log('------------------------------')
+
+  // Retrieve thread contents.
+  for (const [ignored, thread] of Object.entries(threads)) {
+    await page.goto(thread['url'] + '?load_all=1');
+    thread['messages'] = await page.$$eval(
+      'div.arch_kom',
+      (divs) => divs.map((d) => {
+        return {
+          'author': d.parentElement.parentElement.firstChild.textContent,
+          'body': d.textContent
+        }
+      }));
+  }
+  console.log(JSON.stringify(threads));
+
+  // TODO: Read previous threads. Compare by thread ID. Email new messages.
+  // Store these threads if email was successful.
+
+  await browser.close();
+})();
