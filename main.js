2992841fb21cf2676190aaa9944c2e5cbe07ec82
Jörg Zieren <github@zieren.de>
2022-03-11 18:27:31 +0100

    Email is sent

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const nodemailer = require('nodemailer');
 
 const THREADS_FILE = 'kommunikation_fachlehrer.json';
-const CREDENTIALS = JSON.parse(fs.readFileSync('credentials.json', 'utf-8'));
+const CONFIG = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -11,8 +11,8 @@ const CREDENTIALS = JSON.parse(fs.readFileSync('credentials.json', 'utf-8'));
 
   console.log('Logging in...');
   await page.goto('https://theogymuc.eltern-portal.org/');
-  await page.type('#inputEmail', CREDENTIALS.elternportal.email);
-  await page.type('#inputPassword', CREDENTIALS.elternportal.password);
+  await page.type('#inputEmail', CONFIG.elternportal.email);
+  await page.type('#inputPassword', CONFIG.elternportal.password);
   await Promise.all([
     page.click('#inputPassword ~ button'),
     page.waitForNavigation()
@@ -30,38 +30,38 @@ const CREDENTIALS = JSON.parse(fs.readFileSync('credentials.json', 'utf-8'));
         const m = a.href.match(/.*\/([0-9]+)\//);
         const id = m[1];
         return {
-          'id': id,
-          'url': a.href,
-          'name': a.parentElement.parentElement.firstChild.textContent
+          id: id,
+          url: a.href,
+          name: a.parentElement.parentElement.firstChild.textContent
         };
       }));
   // Map of teacher ID to details (name and URL).
   const teachers = {};
   teachersList.forEach((t) => {
-    teachers[t['id']] = t;
-    delete t['id'];
+    teachers[t.id] = t;
+    delete t.id;
   });
 
   // Retrieve metadata for all threads.
   // Map of thread ID to details (list of messages in thread).
   const threads = {};
   for (const [ignored, teacher] of Object.entries(teachers)) {
-    console.log('Reading threads with: ' + teacher['name']);
-    await page.goto(teacher['url']);
+    console.log('Reading threads with: ' + teacher.name);
+    await page.goto(teacher.url);
     const threadsList = await page.$$eval(
       'a[href*="meldungen/kommunikation_fachlehrer/"',
       (anchors) => anchors.map((a) => {
         const m = a.href.match(/.*\/([0-9]+)$/);
         const id = m[1];
         return {
-          'id': id,
-          'url': a.href,
-          'subject': a.textContent
+          id: id,
+          url: a.href,
+          subject: a.textContent
         };
       }));
     threadsList.forEach((t) => {
-      threads[t['id']] = t;
-      delete t['id'];
+      threads[t.id] = t;
+      delete t.id;
     });
   }
 
@@ -69,34 +69,73 @@ const CREDENTIALS = JSON.parse(fs.readFileSync('credentials.json', 'utf-8'));
   console.log('Reading thread contents...');
   for (const [ignored, thread] of Object.entries(threads)) {
     await page.goto(thread['url'] + '?load_all=1');
-    thread['messages'] = await page.$$eval(
+    thread.messages = await page.$$eval(
       'div.arch_kom',
       (divs) => divs.map((d) => {
         return {
-          'author': d.parentElement.parentElement.firstChild.textContent,
-          'body': d.textContent
+          author: d.parentElement.parentElement.firstChild.textContent,
+          body: d.textContent
         };
       }));
   }
 
-  console.log('Emailing new messages...');
+  // Send emails.
+  const emails = [];
+  let emailsFailed = 0;
+  let emailsOK = 0;
   for (const [threadId, thread] of Object.entries(threads)) {
-    if (threadId in previousThreads
-        && previousThreads[threadId]['messages'].length === thread['messages'].length) {
+    const nPrevious =
+        threadId in previousThreads ? previousThreads[threadId]['messages'].length : 0;
+    const messages = thread.messages;
+    const nCurrent = messages.length;
+    if (nCurrent <= nPrevious) {
+      // If messages are ever deleted, we'd need to hash because n could remain constant or
+      // decrease even when new messages are present.
       continue;
     };
-    const messages = thread['messages'];
-    for (let i = previousThreads[threadId]['messages'].length; i < messages.length; ++i) {
-      console.log(messages[i]['body']);
+    for (let i = nPrevious; i < nCurrent; ++i) {
+      emails.push({
+        from: CONFIG.email.from,
+        to: CONFIG.email.to,
+        subject: thread.subject + ' -- ' + messages[i].author,
+        text: messages[i].body
+      });
     }
   };
-
-  // TODO: Conditional on successful email!!
-  console.log('Updating persistent state in ' + THREADS_FILE + '...');
-  fs.writeFileSync(THREADS_FILE, JSON.stringify(threads, null, 2));
-
-  // TODO: Email new messages.
-  // Store these threads if email was successful.
+  // TODO: This runs the risk of flooding. Emails should be throttled. Possibly helpful:
+  // https://www.npmjs.com/package/quota
+  if (emails.length) {
+    console.log('Emailing new messages...');
+    const transport = nodemailer.createTransport({
+      host: CONFIG.email.server,
+      port: 465,
+      secure: true,
+      auth: {
+        user: CONFIG.email.username,
+        pass: CONFIG.email.password
+      }
+    });
+    for (let i = 0; i < emails.length; ++i) {
+      console.log(JSON.stringify(emails[i], null, 2));
+      transport.sendMail(emails[i], function(error, info) {
+        if (error) {
+          console.log('Failed to send email: ' + error);
+          emailsFailed++;
+        } else {
+          console.log('Email sent: ' + info.response);
+          emailsOK++;
+        }
+      });
+    };
+  } else {
+    console.log('No news is good news!');
+  }
+  // TODO: This will get stuck on a query of death and resend everything. Save state with the
+  // failure removed? Remember the failure (hash?), notify about it and skip it next time?
+  if (!emailsFailed) {
+    console.log('Updating persistent state in ' + THREADS_FILE + '...');
+    fs.writeFileSync(THREADS_FILE, JSON.stringify(threads, null, 2));
+  }
 
   await browser.close();
 })();
