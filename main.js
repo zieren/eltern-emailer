885ad4f52dab086d3794d272f3f7fd655e7fd2e8
Jörg Zieren <github@zieren.de>
2023-09-14 13:51:40 +0200

    Set version=0.6.1 (now that it's not changing to an npm module after all)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const TITLE = 'Eltern-Emailer 0.7.0 (c) 2022-2023 Jörg Zieren, GNU GPL v3.'
+const TITLE = 'Eltern-Emailer 0.6.1 (c) 2022-2023 Jörg Zieren, GNU GPL v3.'
     + ' See https://zieren.de/software/eltern-emailer for component license info';
 
 const contentDisposition = require('content-disposition');
