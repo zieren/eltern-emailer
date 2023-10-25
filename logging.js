7cfeb17a9020df502e02fba5554b674e181e5ada
Jörg Zieren <github@zieren.de>
2023-10-25 19:01:27 +0200

    Use system timezone for logging

--- logging.js
+++ logging.js
@@ -7,11 +7,12 @@ global.LOG = null;
 // global.CONFIG (see main.js)
 
 function initialize() {
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
   LOG = winston.createLogger({
     level: CONFIG.options.logLevel,
     format: winston.format.combine(
       winston.format.splat(),
-      winston.format.timestamp(),
+      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss', tz: tz }),
       winston.format.printf(({level, message, timestamp}) => {
         return `${timestamp} ${level}: ${message}`;
       })
@@ -25,6 +26,7 @@ function initialize() {
       new winston.transports.Console()
     ]
   });
+  LOG.debug(`Using timezone "${tz}"`);
 }
 
 module.exports = { initialize };
